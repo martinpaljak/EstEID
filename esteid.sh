d739ea57565d9b8de3ea3bcb00fef7c3e6018fef
Martin Paljak <martin@martinpaljak.net>
2014-02-23 17:15:56 +0000

    File not needed.

--- esteid.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export CLASSPATH=lib/bcpkix-jdk15on-150.jar:lib/bcprov-jdk15on-150.jar:lib/gp.jar:lib/jnasmartcardio.jar:lib/jopt-simple-4.6.jar:$CLASSPATH
-
-java -classpath $CLASSPATH -jar esteid.jar $@
